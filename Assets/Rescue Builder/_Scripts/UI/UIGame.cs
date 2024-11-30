d3a379ecb1d2dd8fb8b5b0ea59391299ab2a7be8
DungNV101 <DungNV101@fpt.com>
2024-11-30 21:24:50 +0700

    setup android platform

---
+++ Assets/Rescue Builder/_Scripts/UI/UIGame.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Watermelon;
+
+namespace RescueProject
+{
+    public class UIGame : UIPage
+    {
+        public override void Initialise()
+        {
+        }
+
+        public override void PlayHideAnimation()
+        {
+        }
+
+        public override void PlayShowAnimation()
+        {
+        }
+    }
+}
